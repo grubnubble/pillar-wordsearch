239d4fb5a4eab039c6361f00a41cd682f7672d6a
grubnubble <montanalisagoodman@gmail.com>
2019-08-21 10:06:26 -0400

    Search through rows forwards and backwards

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ def find_words(csvfile):
 	found_words = []
 
 	# search forward rows
-	found_words.append(search_rows_forward(word_search_list))
+	found_words.append(search_rows(word_search_list))
 
 	return found_words
 
-def search_rows_forward(word_search_list):
+def search_rows(word_search_list):
 	words_to_find = word_search_list.pop(0)
 	found_words = {}
 
 	for row in word_search_list:
 		for word in words_to_find:
-			if word in row:
+			if word in row or word in row[::-1]:
 				found_words[word] = True
 
 	return found_words
--- tests/test_app.py
+++ tests/test_app.py
@@ -7,17 +7,22 @@ def test_convert():
 	expected = "UMKHULKI"
 	assert convert(["U","M","K","H","U","L","K","I"]) == expected
 
-def test_find_words():
-	expected = [{'SCOTTY': True}]
-	assert find_words(CSV_FILE) == expected
-
-def test_search_rows_forward_success():
-	expected = {'SCOTTY': True}
-	word_list = get_rows(CSV_FILE)
-	assert search_rows_forward(word_list) == expected
-
 def test_get_rows():
 	expected = [
 		["BONES","KHAN","KIRK","SCOTTY","SPOCK","SULU","UHURA"],
 		"UMKHULKINVJOCWE", "LLSHKZZWZCGJUYG", "HSUPJPRJDHSBXTG"]
 	assert get_rows(TEST_CSV_FILE)
+
+def test_find_words():
+	expected = [{'SCOTTY': True, 'KIRK': True}]
+	assert find_words(CSV_FILE) == expected
+
+def test_search_rows_success():
+	expected = {'SCOTTY': True, 'KIRK': True}
+	word_list = get_rows(CSV_FILE)
+	assert search_rows(word_list) == expected
+
+def test_search_rows_fail():
+	expected = {}
+	word_list = get_rows(TEST_CSV_FILE)
+	assert search_rows(word_list) == expected
